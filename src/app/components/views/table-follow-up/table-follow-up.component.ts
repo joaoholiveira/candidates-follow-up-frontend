7bdc8c3d283e45fd9deb191661e96df2da84b68f
joao0506 <joaoholiveira1@hotmail.com>
2022-08-29 17:51:42 -0300

    Adicionando paginação na tabel de follow up

--- src/app/components/views/table-follow-up/table-follow-up.component.ts
+++ src/app/components/views/table-follow-up/table-follow-up.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { FollowUp } from 'src/app/model/follow-up.model';
 import { FollowUpService } from 'src/app/services/follow-up.service';
@@ -13,11 +14,18 @@ export class TableFollowUpComponent implements OnInit {
 
   followUps = new MatTableDataSource<FollowUp>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(private followUpService: FollowUpService) { }
 
   ngOnInit(): void {
     this.listarFollowUps();
   }
+
+  ngAfterViewInit() {
+    this.followUps.paginator = this.paginator;
+  }
+
 
   listarFollowUps(){
     this.followUpService.listarFollowUpsHabilitados()
